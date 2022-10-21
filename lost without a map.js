9660a2fc4dba85c25b260b06361c36b7b7576acb
Trenton Sadrakula <tsadrakula@gmail.com>
2022-10-21 02:39:05 -0500

    Vs code is having issues

--- lost without a map.js
+++
@@ -1,5 +0,0 @@
-//Given an array of integers, return a new array with each value doubled.
-
-function maps(x){
-    return x.map((value) => value * 2);
-  };
--- returning string.js
+++
@@ -1,5 +0,0 @@
-//Return a name
-
-function greet(name){
-    return 'Hello, ' + name + ' how are you doing today?'
-  }
