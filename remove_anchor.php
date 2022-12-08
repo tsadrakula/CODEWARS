fa8f87d1e89ca53a9d0ac8e9d478f59323871dd1
Trenton Sadrakula <tsadrakula@gmail.com>
2022-12-07 18:48:46 -0600

    feat: add php and js files

---
+++ remove_anchor.php
@@ -0,0 +1,9 @@
+/*Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"*/
+
+function replaceAll($string) {
+  return preg_replace('/#\S+/', '', $string);
+}
