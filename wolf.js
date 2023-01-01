1b7a9e00efbed9caf56c4bc37acae1103562c388
Trenton Sadrakula <tsadrakula@gmail.com>
2023-01-01 17:34:07 -0600

    featd:add js

---
+++ wolf.js
@@ -0,0 +1,4 @@
+function warnTheSheep(queue) {
+    const position = queue.reverse().indexOf('wolf');
+    return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ position }! You are about to be eaten by a wolf!`;
+  }
