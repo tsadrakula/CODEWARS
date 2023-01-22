e6fbc45b3edabaa335a9be94785e68b0c758cb83
Trenton Sadrakula <tsadrakula@gmail.com>
2023-01-22 16:02:59 -0600

    feat: add js

---
+++ factorials.js
@@ -0,0 +1,11 @@
+function factorial(n){
+    if (n === 0) {
+      return 1
+    }
+  else {    
+    let arr = [];
+    for (let i = 1; i <= n; i++){
+     arr.push(i);
+    } return arr.reduce((acc,curr) => acc * curr);
+  }
+  } 
