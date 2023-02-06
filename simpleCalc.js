2e7bc1d4273e7e53cee2ecddf399aa7c87988c97
Trenton Sadrakula <tsadrakula@gmail.com>
2023-02-06 17:58:58 -0600

    feat: add js

---
+++ simpleCalc.js
@@ -0,0 +1,15 @@
+function calculator(a,b,sign){
+	if ((typeof a === "number") && (typeof b === "number")) {
+    switch (sign) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return a / b;
+    }
+  }
+  return "unknown value";
+}
